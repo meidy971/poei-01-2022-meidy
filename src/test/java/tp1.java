039deb2b4a0df99b4ad4f49f311a95ec84ad30fe
Meidy <meidyjeannoel@icloud.com>
2022-01-05 15:10:53 +0100

    added explicit wait

--- src/test/java/tp1.java
+++ src/test/java/tp1.java
@@ -3,6 +3,8 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -17,9 +19,6 @@ public class tp1 {
     public void setup(){
 
         driver = new ChromeDriver();
-
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
-
         driver.get("https://www.amazon.fr");
         driver.manage().window().maximize();
         WebElement buttonCookies = driver.findElement(By.id("sp-cc-accept"));
@@ -54,4 +53,19 @@ public class tp1 {
         WebElement boutonAjouter = driver.findElement(By.id("add-to-cart-button"));
         boutonAjouter.click();
     }
+
+    @Test
+    public void testExplicitWait() {
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+        
+        driver.findElement(By.id("nav-hamburger-menu")).click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".hmenu-item[data-menu-id='10']")));
+        driver.findElement(By.cssSelector(".hmenu-item[data-menu-id='10']")).click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("ul.hmenu-visible > li:nth-child(3)")));
+        driver.findElement(By.cssSelector("ul.hmenu-visible > li:nth-child(3) > a")).click();
+    }
+
 }
