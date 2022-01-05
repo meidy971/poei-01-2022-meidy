be08f3ab0733fa2aab754ba8f1b57f63092eea39
Meidy <meidyjeannoel@icloud.com>
2022-01-05 10:38:12 +0100

    added setup

--- src/test/java/tp1.java
+++ src/test/java/tp1.java
@@ -3,18 +3,27 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class tp1 {
 
-    @Test
-    public void test1(){
+    WebDriver driver;
+    
+    @BeforeMethod
+    public void setup(){
 
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.get("https://www.amazon.fr");
         driver.manage().window().maximize();
         WebElement buttonCookies = driver.findElement(By.id("sp-cc-accept"));
         buttonCookies.click();
+
+    }
+
+    @Test
+    public void test1(){
+
         WebElement barreRecherche = driver.findElement(By.id("twotabsearchtextbox"));
         barreRecherche.sendKeys("machine à raclette");
         barreRecherche.sendKeys(Keys.ENTER);
@@ -35,11 +44,6 @@ public class tp1 {
     @Test
     public void test2(){
 
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.amazon.fr");
-        driver.manage().window().maximize();
-        WebElement buttonCookies = driver.findElement(By.id("sp-cc-accept"));
-        buttonCookies.click();
         WebElement barreRecherche = driver.findElement(By.id("twotabsearchtextbox"));
         barreRecherche.sendKeys("machine à raclette");
         barreRecherche.sendKeys(Keys.ENTER);
