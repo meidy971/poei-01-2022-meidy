c3a477090f6c9d46632ac1c04b2cde24c895074a
Meidy <meidyjeannoel@icloud.com>
2022-01-07 12:01:45 +0100

    added amazon test file

---
+++ src/test/java/amazonTest.java
@@ -0,0 +1,47 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class amazonTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.get("https://www.amazon.fr");
+        driver.manage().window().maximize();
+
+        // fermer cookies
+        driver.findElement(By.id("sp-cc-accept")).click();
+    }
+
+    @AfterMethod
+    public void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    public void nintendoSwitchAddToCartPriceTest() {
+        // Arrange
+        String productName = "Nintendo Switch OLED";
+        // Act
+        mainPage mainpage = new mainPage(driver);
+        mainpage.searchProduct(productName);
+        // Assert
+    }
+
+    @Test
+    public void machineARacletteTest() {
+        // Arrange
+        String productName = "Machine a raclette";
+
+        // Act
+        mainPage mainPage = new mainPage(driver);
+        mainPage.searchProduct(productName);
+    }
+}
